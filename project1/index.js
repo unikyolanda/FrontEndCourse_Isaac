3f2050124c64cb485bcea7295229b25a79445957
Yi-Ze Huang <b10821056@gmcc.tnua.edu.tw>
2024-10-03 22:39:51 +0800

    loading page 完成

--- project1/index.js
+++ project1/index.js
@@ -13,3 +13,32 @@ document.getElementById("upup").addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+
+document.getElementById("navSearch").addEventListener("click", function () {
+  const searchExpend = document.getElementsByClassName("searchExpend")[0];
+  if (searchExpend.style.display === "block") {
+    searchExpend.style.display = "none";
+    console.log("searchExpend 被關閉了！");
+  } else {
+    searchExpend.style.display = "block";
+    console.log("searchExpend 被打開了！");
+
+    document
+      .getElementById("searchSpace")
+      .addEventListener("click", function () {
+        const searchExpend = document.getElementsByClassName("searchExpend")[0];
+        searchExpend.style.display = "none";
+        console.log("空白空間被關閉");
+      });
+  }
+});
+
+window.addEventListener("load", () => {
+  const loadPage = document.querySelector(".loadpage");
+  setTimeout(() => {
+    loadPage.classList.add("hide");
+  }, 1000);
+  loadPage.addEventListener("transitionend", () => {
+    loadingImg.style.animation = "none";
+  });
+});
