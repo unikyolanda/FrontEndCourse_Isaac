4f7d403343d5bdf1f3d72fe5319cbe1fda1b54e9
Yi-Ze Huang <b10821056@gmcc.tnua.edu.tw>
2024-10-08 15:36:06 +0800

    班級分別點選

--- project1/article/article.js
+++ project1/article/article.js
@@ -1,3 +1,66 @@
+const allopt = document.getElementById("allopt");
+const opt1 = document.getElementById("opt1");
+const opt2 = document.getElementById("opt2");
+const opt3 = document.getElementById("opt3");
+
+fetch("../front-enter-export.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const articlesContainer = document.getElementById("articlemain");
+
+    function displayArticles(classTypeFilter) {
+      articlesContainer.innerHTML = "";
+
+      for (const articleId in data.article) {
+        const article = data.article[articleId];
+        const city = article.city;
+        const name = article.name;
+        const preface = article.preface;
+        const classType = article.classType;
+        const teachWay = article.teachWay;
+        const img = article.squareUrl;
+
+        if (
+          classTypeFilter === "all" ||
+          classType === classTypeFilter ||
+          teachWay === classTypeFilter
+        ) {
+          const articleDiv = document.createElement("div");
+          articleDiv.className = "article";
+
+          articleDiv.innerHTML = `
+        <div class="location">
+          <img src="aritcleimg/location_icon_one.png" class="locationIcon" />
+          <p class="locationText" class="cityFilter">${city}</p>
+        </div>
+        <div class="blockContent">
+          <div class="articleImgFrame"><img class="articleImg" src="${img}"/></div>
+          <h1 class="academy">${name}</h1>
+          <p class="articleDetail">
+          ${preface}
+          </p>
+          <div class="rd">
+            <div class="readmore">readmore</div>
+            <div class="arrowleft"></div>
+          </div>
+        </div>
+        `;
+
+          articlesContainer.appendChild(articleDiv);
+        }
+      }
+    }
+
+    displayArticles("all");
+    allopt.addEventListener("click", () => displayArticles("all"));
+    opt1.addEventListener("click", () => displayArticles("小班制"));
+    opt2.addEventListener("click", () => displayArticles("放養制"));
+    opt3.addEventListener("click", () => displayArticles("一對一"));
+  })
+  .catch((error) => {
+    console.error("Error fetching the JSON data:", error);
+  });
+
 document.getElementById("navSearch").addEventListener("click", function () {
   const searchExpend = document.getElementsByClassName("searchExpend")[0];
   if (searchExpend.style.display === "block") {
@@ -28,4 +91,9 @@ window.addEventListener("load", () => {
     loadingImg.style.animation = "none";
   });
 });
-
+document.getElementById("upup").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
