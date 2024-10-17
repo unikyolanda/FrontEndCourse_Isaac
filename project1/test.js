e8c8f1dad888bf04878bf696591ae8d00815ca7c
Yi-Ze Huang <b10821056@gmcc.tnua.edu.tw>
2024-10-17 23:45:25 +0800

    firebase test web

--- project1/test.js
+++ project1/test.js
@@ -1,11 +1,32 @@
-import { initializeApp } from "htt";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { Firestore, getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseApp = initializeApp();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIJQlCSPgu4yAOnkJkTBnXYf-V1i0z1xQ",
+  authDomain: "frontendcourse-a54bf.firebaseapp.com",
+  projectId: "frontendcourse-a54bf",
+  storageBucket: "frontendcourse-a54bf.appspot.com",
+  messagingSenderId: "12420467790",
+  appId: "1:12420467790:web:f4a330188240610105471f",
+  measurementId: "G-G9Y63GPFK8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
   if (user != null) {
