d9d8f8f2110e6de58eb65c8e07e477aed881aa62
Yi-Ze Huang <b10821056@gmcc.tnua.edu.tw>
2024-10-27 14:14:56 +0800

    testGo點擊的功能做好了

--- project1/common/common.js
+++ project1/common/common.js
@@ -1,9 +1,174 @@
-const q1 = document.getElementById("q1");
-const q2 = document.getElementById("q2");
-const q3 = document.getElementById("q3");
-const q4 = document.getElementById("q4");
-const q5 = document.getElementById("q5");
-const quizResult = document.getElementById("quizResult");
+document.addEventListener("DOMContentLoaded", function () {
+  const testGoGo = document.getElementById("testgogo");
+  if (testGoGo) {
+    const q1 = document.getElementById("q1");
+    const q2 = document.getElementById("q2");
+    const q3 = document.getElementById("q3");
+    const q4 = document.getElementById("q4");
+    const q5 = document.getElementById("q5");
+    const quizResult = document.getElementById("quizResult");
+    testGoGo.addEventListener("click", function () {
+      initializeTestGo();
+      StartQuiz();
+      selectAnswer();
+      CanceledTestGo();
+    });
+  }
+});
+
+function initializeTestGo() {
+  const TestGoDiv = `
+  <div id="quiz">
+  <div id="quizBackground">
+    <div id="quizImg"></div>
+  </div>
+  <div id="quizContent">
+    <div id="intro">
+      <h1 id="starth1">測驗說明</h1>
+      <p class="quizParagraph">
+        點選「開始測驗」後，系統會根據你的回答，找出最適合你的學習環境，並顯示有多少百分比的適合度。
+      </p>
+      <button id="startBtn" class="quizOption">開始測驗</button>
+    </div>
+
+    <div id="q1">
+      <h2 class="quizTitle">選擇在哪個縣市學習？</h2>
+      <p class="quizNumber">1/5</p>
+      <button class="answerOption" data-question="1" data-answer="台北">
+        台北
+      </button>
+      <button class="answerOption" data-question="1" data-answer="台中">
+        台中
+      </button>
+      <button class="answerOption" data-question="1" data-answer="高雄">
+        高雄
+      </button>
+      <button class="answerOption" data-question="1" data-answer="各地">
+        各地
+      </button>
+      <button class="answerOption" data-question="1" data-answer="不重要">
+        不重要
+      </button>
+    </div>
+
+    <div id="q2">
+      <h2 class="quizTitle">每月能撥出多少費用學習？</h2>
+      <p class="quizNumber">2/5</p>
+      <button
+        class="answerOption"
+        data-question="2"
+        data-answer="3000元以下"
+      >
+        3000元以下
+      </button>
+      <button class="answerOption" data-question="2" data-answer="6000元內">
+        6000元內
+      </button>
+      <button
+        class="answerOption"
+        data-question="2"
+        data-answer="10000元內"
+      >
+        10000元內
+      </button>
+      <button
+        class="answerOption"
+        data-question="2"
+        data-answer="10001元以上"
+      >
+        10001元以上
+      </button>
+      <button class="answerOption" data-question="2" data-answer="不重要">
+        不重要
+      </button>
+    </div>
+
+    <div id="q3">
+      <h2 class="quizTitle">每周能撥出多少時間學習？</h2>
+      <p class="quizNumber">4/5</p>
+      <button
+        class="answerOption"
+        data-question="3"
+        data-answer="16小時以下"
+      >
+        16小時以下
+      </button>
+      <button class="answerOption" data-question="3" data-answer="30小時內">
+        30小時內
+      </button>
+      <button class="answerOption" data-question="3" data-answer="45小時內">
+        45小時內
+      </button>
+      <button
+        class="answerOption"
+        data-question="3"
+        data-answer="46小時以上"
+      >
+        46小時以上
+      </button>
+      <button class="answerOption" data-question="3" data-answer="不重要">
+        不重要
+      </button>
+    </div>
+
+    <div id="q4">
+      <h2 class="quizTitle">對班制的需求是？</h2>
+      <p class="quizNumber">4/5</p>
+      <button class="answerOption" data-question="4" data-answer="大班制">
+        大班制
+      </button>
+      <button class="answerOption" data-question="4" data-answer="小班制">
+        小班制
+      </button>
+      <button class="answerOption" data-question="4" data-answer="一對一">
+        一對一
+      </button>
+      <button class="answerOption" data-question="4" data-answer="不重要">
+        不重要
+      </button>
+    </div>
+
+    <div id="q5">
+      <h2 class="quizTitle">喜歡什麼樣的教學方式？</h2>
+      <p class="quizNumber">5/5</p>
+      <button class="answerOption" data-question="5" data-answer="放養制">
+        放養制
+      </button>
+      <button
+        class="answerOption"
+        data-question="5"
+        data-answer="手把手教制"
+      >
+        手把手教制
+      </button>
+      <button class="answerOption" data-question="5" data-answer="不重要">
+        不重要
+      </button>
+    </div>
+
+    <div id="quizResult">
+      <h1 id="starth1">測驗結果</h1>
+      <div id="pie">
+        <div id="bigPie"></div>
+        <div id="smallPie"></div>
+      </div>
+      <div id="resultBtn" ></div>
+    </div>
+  </div>
+</div>
+  `;
+  document.body.insertAdjacentHTML("beforeend", TestGoDiv);
+}
+function CanceledTestGo() {
+  const quiz = document.getElementById("quiz");
+  const quizBackground = document.getElementById("quizBackground");
+  quiz.addEventListener("click", (event) => {
+    // 檢查點擊的目標是否是 quiz 元素本身，且不是 quizBackground 或其子元素
+    if (event.target === quiz && !event.target.closest("#quizBackground")) {
+      quiz.remove();
+    }
+  });
+}
 
 function StartQuiz() {
   const startBtn = document.getElementById("startBtn");
@@ -15,52 +180,54 @@ function StartQuiz() {
     });
   }
 }
-StartQuiz();
 
-const answerOption = document.querySelectorAll(".answerOption"); // 選擇所有測驗按鈕
-let anserAll = {
-  city: "s",
-  fee: "s",
-  weekHour: "s",
-  classType: "s",
-  teachWay: "s",
-};
-if (answerOption) {
-  answerOption.forEach((button) => {
-    button.addEventListener("click", function () {
-      const whichOne = this.getAttribute("data-question");
-      const answer = this.getAttribute("data-answer");
+function selectAnswer() {
+  var anserAll = {
+    city: "s",
+    fee: "s",
+    weekHour: "s",
+    classType: "s",
+    teachWay: "s",
+  };
+  const answerOption = document.querySelectorAll(".answerOption"); // 選擇所有測驗按鈕
 
-      if (whichOne == 1) {
-        anserAll.city = answer;
-        console.log(anserAll);
-        q1.style.display = "none ";
-        q2.style.display = "flex ";
-      } else if (whichOne == 2) {
-        anserAll.fee = answer;
-        q2.style.display = "none ";
-        q3.style.display = "flex ";
-        console.log(anserAll);
-      } else if (whichOne == 3) {
-        anserAll.weekHour = answer;
-        q3.style.display = "none ";
-        q4.style.display = "flex ";
-        console.log(anserAll);
-      } else if (whichOne == 4) {
-        anserAll.classType = answer;
-        q4.style.display = "none ";
-        q5.style.display = "flex ";
-        console.log(anserAll);
-      } else if (whichOne == 5) {
-        anserAll.teachWay = answer;
-        q5.style.display = "none ";
-        quizResult.style.display = "flex ";
-        console.log(anserAll);
-      } else {
-        console.log("選擇有問題");
-      }
+  if (answerOption) {
+    answerOption.forEach((button) => {
+      button.addEventListener("click", function () {
+        const whichOne = this.getAttribute("data-question");
+        const answer = this.getAttribute("data-answer");
+
+        if (whichOne == 1) {
+          anserAll.city = answer;
+          console.log(anserAll);
+          q1.style.display = "none ";
+          q2.style.display = "flex ";
+        } else if (whichOne == 2) {
+          anserAll.fee = answer;
+          q2.style.display = "none ";
+          q3.style.display = "flex ";
+          console.log(anserAll);
+        } else if (whichOne == 3) {
+          anserAll.weekHour = answer;
+          q3.style.display = "none ";
+          q4.style.display = "flex ";
+          console.log(anserAll);
+        } else if (whichOne == 4) {
+          anserAll.classType = answer;
+          q4.style.display = "none ";
+          q5.style.display = "flex ";
+          console.log(anserAll);
+        } else if (whichOne == 5) {
+          anserAll.teachWay = answer;
+          q5.style.display = "none ";
+          quizResult.style.display = "flex ";
+          console.log(anserAll);
+        } else {
+          console.log("選擇有問題");
+        }
+      });
     });
-  });
+  }
 }
 
 /*  Login wrong  */
